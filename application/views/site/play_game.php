a04977b5ccb04ec075e21f7f160b7658b196539e
nishavaish <98401486+nishavaish@users.noreply.github.com>
2022-01-25 19:17:23 +0530

    Update play_game.php

--- application/views/site/play_game.php
+++ application/views/site/play_game.php
@@ -18,8 +18,6 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
   
 	
-   
-	
 	<meta name="mobile-web-app-capable" content="yes">
 	<!--  meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, user-scalable=0"> -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
